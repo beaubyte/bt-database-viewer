bf2ea2d0e604d391d656e5da83fc14705a662d01
Beau <beau@mochabeau.red>
2025-11-20 16:29:05 -0600

    initial work on the search page

--- events.php
+++ events.php
@@ -1,5 +1,4 @@
 <?php
-global $db;
 require_once 'db_connect.php';
 ?>
 <!doctype html>
@@ -10,9 +9,18 @@ require_once 'db_connect.php';
 </head>
 <body>
 <h1><strong>Bluetooth Investigator:</strong></h1>
+    <h3>Search</h3>
+    <form action="search.php" method="post">
+        <p>
+            <label for="name">Name</label>
+            <input type="text" name="name">
+        </p>
+        <input type="submit" class="submit">
 <?php
 $sql = "select * from bluetooth.device_events";
-$statement = $db->query($sql); // fetches all content in Stock and displays on HTML table
+if (isset($db)) {
+    $statement = $db->query($sql);
+}
 echo "<h2>Recent Events:</h2>";
 echo "<div class='options'><table>";
 echo "<tr><td>MAC Address</td><td>Time</td><td>Location</td><td>Name</td><td>Event ID</td></tr>";
@@ -26,41 +34,6 @@ while ($row = $statement->fetch()) {
                 </tr>
                 ";
 }
-/*
-echo "</table></div>";
-$sql = "select * from Requests";
-$statement = $db->query($sql); // fetches all content in Requests and displays on HTML table
-echo "<h2>Pending Orders:</h2>";
-echo "<div class='options'><table>";
-echo "<tr><td>ID</td><td>Time</td><td>Duration</td><td>Location</td><td>Contact Info</td></tr>";
-while ($row = $statement->fetch()) {
-    echo "<tr>
-                <td>$row[ID]</td>
-                <td>$row[Start]</td>
-                <td>$row[Duration] min</td>
-                <td>$row[Location]</td>
-                <td>$row[Contact]</td>
-                </tr>
-                ";
-}
-
-echo "</table></div>";
-$sql = "select * from Payments";
-$statement = $db->query($sql); // fetches all content in Payments and displays on HTML table
-echo "<h2>Purchase History:</h2>";
-echo "<div class='options'><table>";
-echo "<tr><td>ID</td><td>Name</td><td>Card Number</td><td>Time of Purchase</td></tr>";
-while ($row = $statement->fetch()) {
-    echo "<tr>
-                <td>$row[ID]</td>
-                <td>$row[CardName]</td>
-                <td>$row[CardNum]</td>
-                <td>$row[TimeOfPurchase]</td>
-                </tr>
-                ";
-}
-echo "</table></div>";
-*/
 ?>
 </body>
 </html>
--- search.php
+++ search.php
@@ -1,3 +1,26 @@
 <?php
-
+require_once 'db_connect.php';
 ?>
+<!doctype html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<?php
+if (isset($_POST['name'])){
+    $name = $_POST['name']; // gets data from post and assigns to variables
+    $sql = "select * from bluetooth.people p inner join bluetooth.findable f on f.personid = p.personid inner join bluetooth.device_events de on de.macaddress = f.macaddress where p.name = $name;";
+    if (isset($db)) {
+        $statement = $db->query($sql);
+    }
+    echo "<h3>Results for $name</h3>";
+
+
+} else {
+    echo "<h1><strong>Sorry, we couldn't find your order</strong></h1>";
+}
+?>
+</body>
+</html>
